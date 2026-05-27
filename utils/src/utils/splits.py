f291feb2c19e8f50761ceb8dc3acdbd4ff414417
Ted Lentsch <82224705+TedLentsch@users.noreply.github.com>
2026-05-27 16:17:11 +0200

    Update training splits for arXiv and HF

--- utils/src/utils/splits.py
+++ utils/src/utils/splits.py
@@ -1,3 +1,44 @@
+train_mini_splits = {
+    "KITTI360": [
+        "train_scans",
+        "test_scans",
+    ],
+    "nuScenes": "train_scans",
+    "SemanticKITTI": "train_scans",
+    "WaymoPerception": "train_scans__TOP",
+}
+
+train_splits_no_vod = {
+    "AevaScenes": "train_scans",
+    "AV2_Lidar": [
+        "train_scans",
+        "val_scans",
+        "test_scans",
+    ],
+    "KITTI360": [
+        "train_scans",
+        "test_scans",
+    ],
+    "Lyft": [
+        "train_scans__part1",
+        "train_scans__part2",
+        "test_scans",
+    ],
+    "nuScenes": "train_scans",
+    "ONCE": "train_scans",
+    "PandaSet": [
+        "train_scans",
+        "test_scans",
+    ],
+    "SemanticKITTI": "train_scans",
+    "TruckScenes": "train_scans",
+    "WaymoPerception": [
+        "train_scans__TOP",
+        "train_scans__OTHERS",
+    ],
+    "ZOD": "train_scans",
+}
+
 train_splits = {
     "AevaScenes": "train_scans",
     "AV2_Lidar": [
